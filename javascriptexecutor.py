63efdb0cdbf048bdc3c2c453f4a8da29de1beaa3
Rajamegam <rajamegam7@gmail.com>
2024-08-11 12:17:11 +0530

    javascript execution

---
+++ javascriptexecutor.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+
+"""
+1.Running the automation script in headless mode where browser will not invoke and run in headless mode
+2. .ChromeOptions --> class to declare the options in chrome
+3. .add_argument("headless") --> need to tell the browser to invoke in headless mode and we need to tell the same while
+invoking the browser like this --> driver= webdriver.Chrome(options=chromeoptions)
+5. to handle certification (SSL) errors -->options.add_argument("--ignore-certificate-errors") 
+"""
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--ignore-certificate-errors")
+driver = webdriver.Chrome(options=chrome_options)
+
+"""
+.execute_script() --> used to execute js based actions on the web page
+window.scrollBy(0,document.body.scrollHeight)--> is used to go to the bottom of the page
+.scrollHeight --> will take the maximum height of the page and scroll to that position
+"""
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
+# to take screenshot -> here it will scroll to the end of the page and take screenshot
+driver.get_screenshot_as_file("screen.png")
+time.sleep(2)

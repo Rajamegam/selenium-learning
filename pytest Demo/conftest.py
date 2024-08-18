18b87e40fa0141987e787eef35aad437c8e87104
Rajamegam <rajamegam7@gmail.com>
2024-08-18 12:57:30 +0530

    data driven using fixtures

--- pytest Demo/conftest.py
+++ pytest Demo/conftest.py
@@ -7,6 +7,13 @@ def setup():
     yield
     print("I will be run last")
 
+
 @pytest.fixture(scope="class")
 def once():
     print("I will execute only once ")
+
+
+@pytest.fixture()
+def load_data():
+    print(" the value will be passed here for user creation")
+    return ["rajamegam", "govindaraj", "rajamegam7@gmail.com"]

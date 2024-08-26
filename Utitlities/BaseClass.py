9207fe7699e293c9290b632f28ed0d3d55bf2a96
Rajamegam <rajamegam7@gmail.com>
2024-08-26 21:57:12 +0530

    changes

--- Utitlities/BaseClass.py
+++ Utitlities/BaseClass.py
@@ -9,6 +9,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
+
+
     def verify_link_presence(self,text):
         wait = WebDriverWait(self.driver, 10)
         wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, text)))
@@ -17,7 +19,7 @@ class BaseClass:
         sel = Select(locator)
         sel.select_by_visible_text(text)
 
-    def getlogger(self):
+    def getLogger(self):
         logger = logging.getLogger(__name__)
         filehandler = logging.FileHandler('logfile.log')
         error_log_format = logging.Formatter("%(asctime)s: %(levelname)s: %(name)s: %(message)s")
@@ -25,9 +27,9 @@ class BaseClass:
         logger.addHandler(filehandler)
 
         logger.setLevel(logging.INFO)
-        logger.debug("This is a debug error")
-        logger.info("THis is a info error")
-        logger.warning("This is a warning error")
-        logger.error("This is a error")
-        logger.critical("This is a critical error")
+        # logger.debug("This is a debug error")
+        # logger.info("THis is a info error")
+        # logger.warning("This is a warning error")
+        # logger.error("This is a error")
+        # logger.critical("This is a critical error")
         return logger

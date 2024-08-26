dac07b2e210b827099bb012ac78456c5f1ac32fe
Rajamegam <rajamegam7@gmail.com>
2024-08-26 07:22:52 +0530

    parameterization implemented

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -7,17 +8,23 @@ from pageObjects.HomePage import HomePage
 
 
 class TestFormSubmission(BaseClass):
-    def test_form_submission(self):
+    def test_form_submission(self, getData):
         homepage = HomePage(self.driver)
-        homepage.name().send_keys("Rajamegam")
-        homepage.email().send_keys("rajamegam7@gmail.com")
-        homepage.password().send_keys("Test@123")
+        homepage.name().send_keys(getData[0])
+        homepage.email().send_keys(getData[1])
+        homepage.password().send_keys(getData[2])
         homepage.checkbox().click()
         # gender_dropdown = Select(homepage.gender())
         # gender_dropdown.select_by_visible_text("Male")
-        self.select_option_by_text(homepage.gender(),"Male")
+        self.select_option_by_text(homepage.gender(), getData[3])
         homepage.radio_button().click()
-        homepage.birthday().send_keys("11/16/1995")
+        homepage.birthday().send_keys(getData[4])
         homepage.submit_button().click()
         success_message = homepage.success_message().text
         assert "Success!" in success_message
+        self.driver.refresh()
+
+    @pytest.fixture(params=[("Rajamegam", "rajamegam7@gmail.com", "Test@123", "Male", "11/16/1995"),
+                            ("Priya", "Priya@gmail.com", "Test@123", "Female", "02/23/1995")])
+    def getData(self, request):
+        return request.param
